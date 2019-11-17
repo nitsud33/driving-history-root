62ea54562f9275bcb7a76eb4e48e0d94b07d8e94
Dustin Bixler <nitsud33@gmail.com>
2019-11-17 13:26:25 -0500

    init commit

---
+++ src/main/java/com/root/drivinghistory/DrivingHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.root.drivinghistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/root/drivinghistory/DrivingHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.drivinghistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
