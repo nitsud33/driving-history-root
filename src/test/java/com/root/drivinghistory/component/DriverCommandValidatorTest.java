96741c5700218822956bd5f5e3ab9aabf111bc96
Dustin Bixler <nitsud33@gmail.com>
2019-11-17 14:16:11 -0500

    more driver command negative validation tests

--- src/test/java/com/root/drivinghistory/component/DriverCommandValidatorTest.java
+++ src/test/java/com/root/drivinghistory/component/DriverCommandValidatorTest.java
@@ -12,4 +12,28 @@ public class DriverCommandValidatorTest {
     public void null_line_is_invalid () {
         assertThat(driverCommandValidator.isValidDriverCommandLine(null)).isFalse();
     }
+
+    @Test
+    public void empty_line_is_invalid () {
+        assertThat(driverCommandValidator.isValidDriverCommandLine("")).isFalse();
+    }
+
+    @Test
+    public void blank_line_is_invalid () {
+        assertThat(driverCommandValidator.isValidDriverCommandLine("    ")).isFalse();
+    }
+
+    @Test
+    public void line_must_start_with_Driver () {
+        assertThat(driverCommandValidator.isValidDriverCommandLine("Add Dan")).isFalse();
+        assertThat(driverCommandValidator.isValidDriverCommandLine("Add Driver Dan ")).isFalse();
+        assertThat(driverCommandValidator.isValidDriverCommandLine("DDriver Dan ")).isFalse();
+    }
+
+    @Test
+    public void Driver_without_name_is_invalid () {
+        assertThat(driverCommandValidator.isValidDriverCommandLine("Driver")).isFalse();
+        assertThat(driverCommandValidator.isValidDriverCommandLine("Driver ")).isFalse();
+    }
+
 }
