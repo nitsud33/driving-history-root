f0ba6b5ddc7cc9a9cbdb50c96ee81b6df3de5f6f
Dustin Bixler <nitsud33@gmail.com>
2019-11-17 14:38:32 -0500

    add getDriverName function and tests

--- src/main/java/com/root/drivinghistory/component/DriverCommandValidator.java
+++ src/main/java/com/root/drivinghistory/component/DriverCommandValidator.java
@@ -1,12 +1,28 @@
 package com.root.drivinghistory.component;
 
+import java.util.*;
+
+import static java.util.Arrays.asList;
+
 public class DriverCommandValidator {
+
+    public String getDriverName(String line) {
+        if (isValidDriverCommandLine(line)){
+            List<String> words = asList(line.split("\\s+"));
+            return words
+                    .subList(1, words.size())
+                    .stream()
+                    .reduce((a,b) -> a + " " + b)
+                    .orElse(null);
+        }
+        return null;
+    }
 
     public boolean isValidDriverCommandLine(String line) {
         if (line == null) {
             return false;
         }
-        String[] words = line.split(" ");
+        String[] words = line.split("\\s+");
         return words.length > 1 && words[0].equals("Driver");
     }
 }
--- src/test/java/com/root/drivinghistory/component/DriverCommandValidatorTest.java
+++ src/test/java/com/root/drivinghistory/component/DriverCommandValidatorTest.java
@@ -11,16 +11,19 @@ public class DriverCommandValidatorTest {
     @Test
     public void null_line_is_invalid () {
         assertThat(driverCommandValidator.isValidDriverCommandLine(null)).isFalse();
+        assertThat(driverCommandValidator.getDriverName(null)).isNull();
     }
 
     @Test
     public void empty_line_is_invalid () {
         assertThat(driverCommandValidator.isValidDriverCommandLine("")).isFalse();
+        assertThat(driverCommandValidator.getDriverName("")).isNull();
     }
 
     @Test
     public void blank_line_is_invalid () {
         assertThat(driverCommandValidator.isValidDriverCommandLine("    ")).isFalse();
+        assertThat(driverCommandValidator.getDriverName("    ")).isNull();
     }
 
     @Test
@@ -28,23 +31,41 @@ public class DriverCommandValidatorTest {
         assertThat(driverCommandValidator.isValidDriverCommandLine("Add Dan")).isFalse();
         assertThat(driverCommandValidator.isValidDriverCommandLine("Add Driver Dan ")).isFalse();
         assertThat(driverCommandValidator.isValidDriverCommandLine("DDriver Dan ")).isFalse();
+
+        assertThat(driverCommandValidator.getDriverName("Add Dan")).isNull();
+        assertThat(driverCommandValidator.getDriverName("Add Driver Dan ")).isNull();
+        assertThat(driverCommandValidator.getDriverName("DDriver Dan ")).isNull();
     }
 
     @Test
     public void Driver_without_name_is_invalid () {
         assertThat(driverCommandValidator.isValidDriverCommandLine("Driver")).isFalse();
         assertThat(driverCommandValidator.isValidDriverCommandLine("Driver ")).isFalse();
+
+        assertThat(driverCommandValidator.getDriverName("Driver")).isNull();
+        assertThat(driverCommandValidator.getDriverName("Driver ")).isNull();
     }
 
     @Test
     public void Driver_with_name_is_valid () {
         assertThat(driverCommandValidator.isValidDriverCommandLine("Driver Dan")).isTrue();
+        assertThat(driverCommandValidator.getDriverName("Driver Dan")).isEqualTo("Dan");
         assertThat(driverCommandValidator.isValidDriverCommandLine("Driver Donkey Kong")).isTrue();
+        assertThat(driverCommandValidator.getDriverName("Driver Donkey Kong")).isEqualTo("Donkey Kong");
     }
 
     @Test
     public void validator_ignores_extra_spaces_in_between_words () {
         assertThat(driverCommandValidator.isValidDriverCommandLine("Driver    Donkey   Kong")).isTrue();
+        assertThat(driverCommandValidator.getDriverName("Driver    Donkey   Kong")).isEqualTo("Donkey Kong");
+    }
+
+    @Test
+    public void driver_can_have_name_driver () {
+        assertThat(driverCommandValidator.isValidDriverCommandLine("Driver Driver")).isTrue();
+        assertThat(driverCommandValidator.getDriverName("Driver Driver")).isEqualTo("Driver");
+        assertThat(driverCommandValidator.isValidDriverCommandLine("Driver Is Driver Donkey Kong")).isTrue();
+        assertThat(driverCommandValidator.getDriverName("Driver Is Driver Donkey Kong")).isEqualTo("Is Driver Donkey Kong");
     }
 
 }
