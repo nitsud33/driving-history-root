7a07df5e0e2ef1df09da6dacf58fcfbeca8f3486
Dustin Bixler <nitsud33@gmail.com>
2019-11-17 20:15:02 -0500

    add happy path trip repo tests

--- src/main/java/com/root/drivinghistory/repository/TripRepository.java
+++ src/main/java/com/root/drivinghistory/repository/TripRepository.java
@@ -4,15 +4,28 @@ import com.root.drivinghistory.*;
 
 import java.util.*;
 
+import static org.apache.logging.log4j.util.Strings.isNotBlank;
+
 public class TripRepository {
 
     List<Trip> trips = new ArrayList<>();
 
     public void save(Trip trip){
-
+        if (isValidTrip(trip)){
+            trips.add(trip);
+        }
     }
 
     public List<Trip> getAll(){
         return trips;
+    }
+
+    private boolean isValidTrip(Trip trip) {
+        return trip != null &&
+                isNotBlank(trip.getDriverName()) &&
+                trip.getTime() != null &&
+                trip.getTime() >= 0 &&
+                trip.getDistance() != null &&
+                trip.getDistance() >= 0.0;
     }
 }
--- src/test/java/com/root/drivinghistory/repository/TripRepositoryTest.java
+++ src/test/java/com/root/drivinghistory/repository/TripRepositoryTest.java
@@ -3,6 +3,9 @@ package com.root.drivinghistory.repository;
 import com.root.drivinghistory.*;
 import org.junit.jupiter.api.*;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.apache.commons.lang3.RandomUtils.nextDouble;
+import static org.apache.commons.lang3.RandomUtils.nextInt;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TripRepositoryTest {
@@ -38,6 +41,30 @@ public class TripRepositoryTest {
         repo.save(new Trip("Donkey Kong", 1, null));
         repo.save(new Trip("Donkey Kong", 1, -1.0));
         assertThat(repo.getAll()).isEmpty();
+    }
+
+    @Test
+    public void will_save_single_trip() {
+        Trip trip = new Trip(randomAlphanumeric(8), nextInt(), nextDouble());
+        repo.save(trip);
+        assertThat(repo.getAll()).containsExactly(trip);
+    }
+
+    @Test
+    public void will_save_multiple_trips() {
+        Trip trip1 = new Trip(randomAlphanumeric(8), nextInt(), nextDouble());
+        Trip trip2 = new Trip(randomAlphanumeric(8), nextInt(), nextDouble());
+        repo.save(trip1);
+        repo.save(trip2);
+        assertThat(repo.getAll()).containsExactly(trip1, trip2);
+    }
+
+    @Test
+    public void will_save_duplicate_trips() {
+        Trip trip1 = new Trip(randomAlphanumeric(8), nextInt(), nextDouble());
+        repo.save(trip1);
+        repo.save(trip1);
+        assertThat(repo.getAll()).containsExactly(trip1, trip1);
     }
 
 }
