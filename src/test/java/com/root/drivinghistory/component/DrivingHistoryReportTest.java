1c10b6b5d7bbdf504cfee44fec9d081b3657228f
Dustin Bixler <nitsud33@gmail.com>
2019-11-18 20:50:16 -0500

    add test from code kata

--- src/test/java/com/root/drivinghistory/component/DrivingHistoryReportTest.java
+++ src/test/java/com/root/drivinghistory/component/DrivingHistoryReportTest.java
@@ -25,7 +25,7 @@ public class DrivingHistoryReportTest {
     );
 
     @Test
-    public void reportOneDriver(){
+    public void reportOneDriver() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 10"
@@ -38,7 +38,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void reportOneDriverWithNoTrips(){
+    public void reportOneDriverWithNoTrips() {
         String[] lines = new String[]{
                 "Driver Dan",
         };
@@ -50,7 +50,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_ignores_trips_less_than_5mph(){
+    public void report_ignores_trips_less_than_5mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 4.99999"
@@ -63,7 +63,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_accepts_trips_at_5mph(){
+    public void report_accepts_trips_at_5mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 5"
@@ -76,7 +76,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_ignores_trips_greater_than_100mph(){
+    public void report_ignores_trips_greater_than_100mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 100.00001"
@@ -89,7 +89,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_accepts_trips_at_100mph(){
+    public void report_accepts_trips_at_100mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 100"
@@ -102,7 +102,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_rounds_5_point_4999mph_down_to_5mph(){
+    public void report_rounds_5_point_4999mph_down_to_5mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 5.4999"
@@ -115,7 +115,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_rounds_5_point_5mph_up_to_6mph(){
+    public void report_rounds_5_point_5mph_up_to_6mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 5.5"
@@ -128,7 +128,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_rounds_10_point_49999_miles_down_to_10_miles(){
+    public void report_rounds_10_point_49999_miles_down_to_10_miles() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 10.49999"
@@ -141,7 +141,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_rounds_10_point_5_miles_up_to_11_miles(){
+    public void report_rounds_10_point_5_miles_up_to_11_miles() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 10.5"
@@ -154,7 +154,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_shows_100_miles_in_10_hours_as_10mph(){
+    public void report_shows_100_miles_in_10_hours_as_10mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 03:33 13:33 100"
@@ -167,7 +167,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void trip_of_5_miles_in_1_hour_and_95_miles_in_9_hours_is_100_miles_total_and_10mph(){
+    public void trip_of_5_miles_in_1_hour_and_95_miles_in_9_hours_is_100_miles_total_and_10mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 5",
@@ -181,7 +181,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void trip_of_1_mile_in_1_min_is_60mph(){
+    public void trip_of_1_mile_in_1_min_is_60mph() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 00:01 1",
@@ -194,7 +194,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void trip_of_10_point_4999_miles_and_10_point_4999_miles_rounds_to_21_miles(){
+    public void trip_of_10_point_4999_miles_and_10_point_4999_miles_rounds_to_21_miles() {
         String[] lines = new String[]{
                 "Driver Dan",
                 "Trip Dan 00:00 01:00 10.4999",
@@ -208,7 +208,7 @@ public class DrivingHistoryReportTest {
     }
 
     @Test
-    public void report_sorts_50miles_above_49miles_above_48miles(){
+    public void report_sorts_50miles_above_49miles_above_48miles() {
         String[] lines = new String[]{
                 "Driver DonkeyKong",
                 "Driver DiddyKong",
@@ -228,4 +228,22 @@ public class DrivingHistoryReportTest {
         );
     }
 
+    @Test
+    public void test_that_my_product_owner_wanted_me_to_run() {
+        String[] lines = new String[]{
+                "Driver Dan",
+                "Driver Lauren",
+                "Driver Kumi",
+                "Trip Dan 07:15 07:45 17.3",
+                "Trip Dan 06:12 06:32 21.8",
+                "Trip Lauren 12:01 13:16 42.0"
+        };
+
+        drivingHistoryReport.parse(lines);
+        assertThat(drivingHistoryReport.report()).containsExactly(
+                "Lauren: 42 miles @ 34 mph",
+                "Dan: 39 miles @ 47 mph",
+                "Kumi: 0 miles"
+        );
+    }
 }
